62afa4152631a2cb99c3fbaca677100e576f42fe
salil <salil.saifuddin@netspective.org>
2024-01-29 17:22:09 +0530

    fix: add port and ip enable debug

--- app.py
+++ app.py
@@ -229,5 +229,5 @@ def root():
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0') 
+    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(host='0.0.0.0') 
